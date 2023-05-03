1ae974aa7e33d57daf737b3f3b3ce289e40a9a1e
Madamered27 <rinaldi.stefania09@gmail.com>
2023-05-03 02:18:15 -0300

    Creacion carpeta layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AgregarNuevoProductoComponent } from './componentes/agregar-nuevo-producto/agregar-nuevo-producto.component';
+import { LayoutComponent } from './layout/layout.component';
 
 const routes: Routes = [
-  {path: "", redirectTo: "", pathMatch: "full"},
+  {path: "", redirectTo: "layout", pathMatch: "full"},
+  {path:"layout", component: LayoutComponent},
   {path:"carrito", component:AgregarNuevoProductoComponent}, //se tiene que cambiar por el componente de carrito
   {path:"agregarProducto", component:AgregarNuevoProductoComponent},
   {path:"**", redirectTo: "", pathMatch: "full"}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,11 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AgregarNuevoProductoComponent } from './componentes/agregar-nuevo-producto/agregar-nuevo-producto.component';
-import { NavbarComponent } from './componentes/navbar/navbar.component';
-import { FooterComponent } from './componentes/footer/footer.component';
+import { NavbarComponent } from './layout/navbar/navbar.component';
+import { FooterComponent } from './layout/footer/footer.component';
 import { FormularioComponent } from './componentes/formulario/formulario.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { LayoutComponent } from './layout/layout.component';
 
 @NgModule({
   declarations: [
@@ -17,7 +18,8 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     AgregarNuevoProductoComponent,
     NavbarComponent,
     FooterComponent,
-    FormularioComponent
+    FormularioComponent,
+    LayoutComponent
   ],
   imports: [
     BrowserModule,
