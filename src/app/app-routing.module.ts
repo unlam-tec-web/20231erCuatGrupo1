1a8136e8c6a510e9d1b6a51f3918ecf04b1878be
Luis Agustin Vega Dobal <lvegadobal@alumno.unlam.edu.ar>
2023-05-03 08:47:07 -0300

    correccion de carpetas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {CarritoDeComprasComponent} from "./componentes/carrito-de-compras/carrito-de-compras.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'cart', component: CarritoDeComprasComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CarritoDeComprasComponent } from './carrito-de-compras/carrito-de-compras.component';
+import { CarritoDeComprasComponent } from './componentes/carrito-de-compras/carrito-de-compras.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { NavbarComponent } from './layout/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     CarritoDeComprasComponent,
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
