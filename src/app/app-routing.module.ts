987d2aed5a41c247d44d5f338b15112104a7f683
agustinnava0 <agustinnava0@gmail.com>
2023-05-06 02:53:50 -0300

    vista login y registro

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { AgregarNuevoProductoComponent } from './componentes/agregar-nuevo-producto/agregar-nuevo-producto.component';
 import { LayoutComponent } from './layout/layout.component';
 import { HomeComponent } from './componentes/home/home.component';
+import { SingInComponent } from './componentes/sing-in/sing-in.component';
+import { SingUpComponent } from './componentes/sing-up/sing-up.component';
 
 const routes: Routes = [
   {path: "", redirectTo: "home", pathMatch: "full"},
   {path:"layout", component: LayoutComponent},
+  {path:"singIn", component: SingInComponent},
+  {path:"singUp", component: SingUpComponent},
   {path:"home", component: HomeComponent},
   {path:"carrito", component:AgregarNuevoProductoComponent}, //se tiene que cambiar por el componente de carrito
   {path:"agregarProducto", component:AgregarNuevoProductoComponent},
