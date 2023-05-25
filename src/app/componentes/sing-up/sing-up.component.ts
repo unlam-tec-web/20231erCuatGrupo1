3588f4a260c7afdf3aa33dcb684096c2b6d8c0fd
agustinnava0 <agustinnava0@gmail.com>
2023-05-25 04:06:24 -0300

    validaciones sing up

--- src/app/componentes/sing-up/sing-up.component.ts
+++ src/app/componentes/sing-up/sing-up.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-sing-up',
@@ -6,6 +7,50 @@ import { Component } from '@angular/core';
   styleUrls: ['./sing-up.component.css']
 })
 export class SingUpComponent {
-  passwordVisible = false;
-  verifyPasswordVisible = false;
+
+  ngOnInit(): void{
+  }
+
+  showPassword = false;
+  showConfirmPassword = false;
+
+  passwordVisibility(field: string) {
+    if (field === 'password') {
+      this.showPassword = !this.showPassword;
+    } else if (field === 'confirmPassword') {
+      this.showConfirmPassword = !this.showConfirmPassword;
+    }
+  }
+
+  formSingup: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.formSingup = this.formBuilder.group({
+      firstName: ['', Validators.required],
+      lastName:  ['', Validators.required],
+      address: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]],
+      confirmPassword: ['', [Validators.required]]
+    });
+  }
+
+  get firstNameInvalid(): boolean {
+    const control = this.formSingup.controls['firstName'];
+    return control.invalid && control.touched;
+  }
+
+  get lastNameInvalid(): boolean {
+    const control = this.formSingup.controls['lastName'];
+    return control.invalid && control.touched;
+  }
+
+  onSubmit() {
+
+    if (this.formSingup.invalid) {
+      
+    }
+
+  }
+
 }
