1cf9c012458d789b870c996d22a055fb7be8c51a
Madamered27 <rinaldi.stefania09@gmail.com>
2023-06-17 12:39:37 -0300

    se agrego la logica para mostrar los productos

--- src/app/componentes/card/card.component.ts
+++ src/app/componentes/card/card.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Router } from '@angular/router';
+import { ProductoService } from 'src/app/services/producto.service';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.css']
 })
-export class CardComponent {
+export class CardComponent implements OnInit{
+
+  @Input()
+  producto:any;
+  precio: number = 0;
+  nombre: string = "";
+  id: number = 0;
+  @Output()
+  onEliminar: EventEmitter <number> = new EventEmitter();
+
+  constructor(private router: Router, private productoService: ProductoService){
+  }
+
+  ngOnInit(): void {
+    this.nombre = this.producto.nombre;
+    this.id = this.producto.id;
+    this.precio = this.producto.precio;
+
+  }
+
+  verDetalleProducto(): void{
+    this.router.navigate(['producto', this.id]);
+  }
+
+  eliminarProducto(): void{
+    this.productoService.eliminarProducto(this.id).subscribe(resp =>{
+      console.log(resp);
+    })
+    this.onEliminar.emit(this.id);
+  }
+
+  editarProducto(){
+    this.router.navigate(['editar', this.id]);
+  }
 
 }
--- src/app/componentes/home/home.component.ts
+++ src/app/componentes/home/home.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductoService } from 'src/app/services/producto.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+
+  listProductos: Array<any> = []
+  isCargando: boolean = true;
+
+  constructor(private productoService: ProductoService){}
+
+  ngOnInit(): void {
+    this.getProductos();
+  }
+
+  getProductos(): void{
+    this.productoService.getProductos().subscribe(productos =>{
+      this.listProductos = productos
+      this.isCargando = false;
+    })
+  }
+
+  eliminarCard(id: number){
+    this.listProductos = this.listProductos.filter(producto => producto.id !== id);
+  }
 
 }
