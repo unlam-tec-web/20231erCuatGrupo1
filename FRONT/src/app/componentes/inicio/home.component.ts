fe5e77c50c1406d290005461f336e34d5461a48b
agustinnava0 <agustinnava0@gmail.com>
2023-06-21 03:52:44 -0300

    imagenes banner

--- FRONT/src/app/componentes/inicio/home.component.ts
+++ FRONT/src/app/componentes/inicio/home.component.ts
@@ -8,14 +8,20 @@ import { CategoriaService } from 'src/app/services/categoria.service';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit{
+export class HomeComponent implements OnInit {
 
   listMarcas: Array<any> = []
   listProductos: Array<any> = []
   listCategorias: Array<any> = []
   isCargando: boolean = true;
 
-  constructor(private productoService: ProductoService, private marcaService: MarcaService, private categoriaService: CategoriaService){}
+  bannerImages: string[] = [
+    "../../assets/img/banner1.jpg",
+    "../../assets/img/banner2.jpeg",
+    "../../assets/img/banner3.jpeg"
+  ];
+
+  constructor(private productoService: ProductoService, private marcaService: MarcaService, private categoriaService: CategoriaService) { }
 
   ngOnInit(): void {
     this.getProductos();
@@ -23,22 +29,22 @@ export class HomeComponent implements OnInit{
     this.getCategorias();
   }
 
-  getProductos(): void{
-    this.productoService.getProductos().subscribe(productos =>{
+  getProductos(): void {
+    this.productoService.getProductos().subscribe(productos => {
       this.listProductos = productos
       this.isCargando = false;
     })
   }
 
-  getMarcas(): void{
-    this.marcaService.getMarcas().subscribe(marcas =>{
+  getMarcas(): void {
+    this.marcaService.getMarcas().subscribe(marcas => {
       this.listMarcas = marcas
       this.isCargando = false;
     })
   }
 
-  getCategorias(): void{
-    this.categoriaService.getCategorias().subscribe(categorias =>{
+  getCategorias(): void {
+    this.categoriaService.getCategorias().subscribe(categorias => {
       this.listCategorias = categorias
       this.isCargando = false;
     })
