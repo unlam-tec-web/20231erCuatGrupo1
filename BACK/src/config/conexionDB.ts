084772632da0ae16928763720421b886ea2e0b1b
Madamered27 <rinaldi.stefania09@gmail.com>
2023-06-27 00:31:24 -0300

    se deja de hacer seguimiento al archivo de conexion a bd

---
+++ BACK/src/config/conexionDB.ts
@@ -0,0 +1,18 @@
+import { DataSource } from "typeorm";
+//import {Usuario} from "../entities/Usuario";
+import { Marca } from "../entities/Marca";
+import { Producto } from "../entities/Producto";
+import { Categoria } from "../entities/Categoria";
+
+export const AppDataSource = new DataSource({
+    type:"mysql",
+    host:'localhost',
+    username: 'root', 
+    password: 'root',
+    port: 3306,
+    database: 'grupo-1',
+    entities: [Producto, Marca, Categoria],
+    //entities: [Usuario, Producto],
+    logging:true,
+    synchronize: true
+})
